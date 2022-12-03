e25d5960318027b38d15dafdfa93fe42e09e3e50
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-12-03 19:13:37 -0300

    feat: configuring socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
+import { connectToSocket } from './socket';
+import path from 'path';
 
 config();
 
 const app = express();
+const { serverHttp } = connectToSocket(app);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-export { app };
+export { serverHttp };
